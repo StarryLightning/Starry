13dd7be5600f0be82925dff7cf5e429a57eda974
StarryLightning <91296858+StarryLightning@users.noreply.github.com>
2021-09-28 22:41:46 +0800

    Add files via upload

---
+++ 计算圆的周长与面积.cpp
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+	double r;
+	double c;
+	double s;
+	const double PI=3.14159;
+	printf("input r:\n");
+	scanf("%lf",&r);
+	printf("c=%lf\n",2*PI*r);
+	printf("s=%lf\n",PI*r*r);
+	return 0;
+	
+}
